bd2b59a30f4bc1117369b843cadebc47c125939e
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-08 19:43:05 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,4 @@
 # Add your variables here
-do
-first_number = get_variable_from_file('./calculator.rb', "first_number")
-expect(first_number).to be_an(Integer).or be_a(Float)
-end
 
-expect(second_number).to be_an(Integer).or be_a(Float)
-expect(second_number).not_to equal(0)
-end
+first_number = 5
+second_number = 5
